84b2b797205f7b117c480edfb8a047ffe5d358e5
Marco Romanelli <mromanelli9@gmail.com>
2017-07-19 12:21:50 +0200

    add generation of mobility file; some fix.

--- scripts/generate-sumo-files.sh
+++ scripts/generate-sumo-files.sh
@@ -1,33 +1,75 @@
 #!/bin/bash
 
+SIMULATION_END_TIME=50
+
 OSM_FILE=$1
 BASENAME=$(basename "$OSM_FILE" .osm.xml)
 
+NETCONVERT_CC="$(which netconvert)"
+POLYCONVERT_CC="$(which polyconvert)"
+RANDOM_TRIPS_CC="$(which sumo-randomTrips)"
+SUMO_CC="$(which sumo)"
+TRACE_EXPORTER="$(which sumo-traceExporter)"
+
+# Check if software exitsts
+I="0"
+if [ "$NETCONVERT_CC" == "" ]; then
+	echo "[!] Error: netconvert not found."
+	I=$((I + 1))
+fi
+if [ "$POLYCONVERT_CC" == "" ]; then
+	echo "[!] Error: polyconvert not found."
+	I=$((I + 1))
+fi
+if [ "$RANDOM_TRIPS_CC" == "" ]; then
+	echo "[!] Error: randomTrips not found."
+	I=$((I + 1))
+fi
+if [ "$SUMO_CC" == "" ]; then
+	echo "[!] Error: sumo not found."
+	I=$((I + 1))
+fi
+if [ "$TRACE_EXPORTER" == "" ]; then
+	echo "[!] Error: traceExporter not found."
+	I=$((I + 1))
+fi
+
+if [ "$I" != "0" ]; then
+	echo
+	exit 2
+fi
+
+
+# Generate data
 NET_FILE="$BASENAME.net.xml"
 BUILDINGS_FILE="$BASENAME.buildings.xml"
-ROUTES_FILE="$BASENAME.rou.xml"
+ROUTES_FILE="$BASENAME.trips.xml"
 SUMO_CFG_FILE="$BASENAME.sumo.cfg"
+TRACE_FILE="$BASENAME.trace.xml"
+MOBILITY_FILE="$BASENAME.ns2mobility.tcl"
 
-SIMULATION_END_TIME=20
 
 # Generate network
-netconvert --osm-files="$OSM_FILE" -o "$NET_FILE"
+$NETCONVERT_CC --osm-files="$OSM_FILE" -o "$NET_FILE" &&
 
 # Generate buildings
-polyconvert --osm-files="$OSM_FILE" -o "$BUILDINGS_FILE"
+$POLYCONVERT_CC --osm-files="$OSM_FILE" -o "$BUILDINGS_FILE" &&
 
 # Generate routes
-/opt/local/bin/sumo-randomTrips -n "$NET_FILE" -e "$SIMULATION_END_TIME" -o "$ROUTES_FILE"
+$RANDOM_TRIPS_CC -n "$NET_FILE" -e "$SIMULATION_END_TIME" -o "$ROUTES_FILE" &&
 
 
 # Generate config file
 cat >> "$SUMO_CFG_FILE" <<EOF
 <configuration>
-    <OSM_FILE>
+    <input
         <net-file value="$NET_FILE"/>
         <route-files value="$ROUTES_FILE"/>
         <additional-files value="$BUILDINGS_FILE"/>
-    </OSM_FILE>
+    </input>
+		<output>
+			<fcd-output value="$TRACE_FILE" />
+		</output>
     <time>
         <begin value="0"/>
         <end value="$SIMULATION_END_TIME"/>
@@ -35,5 +77,9 @@ cat >> "$SUMO_CFG_FILE" <<EOF
 </configuration>
 EOF
 
+$SUMO_CC -c "$SUMO_CFG_FILE" 2&>/dev/null &&
+$TRACE_EXPORTER -i "$TRACE_FILE" --ns2mobility-out "$MOBILITY_FILE"
+
 # Exit
+echo
 exit 0
