f9ac57ac382f32b2f0ded8043df2d216aa98aeaa
Marco Romanelli <mromanelli9@gmail.com>
2017-07-26 11:08:36 +0200

    continue copying part of Griglia; up to outputs

--- ns-3/simulations/fb-vanet/fb-vanet.cc
+++ ns-3/simulations/fb-vanet/fb-vanet.cc
@@ -53,23 +53,23 @@ class FBVanetExperiment
 {
 public:
 	/**
-   * \brief Constructor
-   * \return none
-   */
+	 * \brief Constructor
+	 * \return none
+	 */
 	FBVanetExperiment ();
 
 	/**
-   * \brief Destructor
-   * \return none
-   */
+	 * \brief Destructor
+	 * \return none
+	 */
 	virtual ~FBVanetExperiment ();
 
 	/**
-   * \brief Enacts simulation of an ns-3  application
-   * \param argc program arguments count
-   * \param argv program arguments
-   * \return none
-   */
+	 * \brief Enacts simulation of an ns-3  application
+	 * \param argc program arguments count
+	 * \param argv program arguments
+	 * \return none
+	 */
 	void Simulate (int argc, char **argv);
 
 protected:
@@ -82,28 +82,28 @@ protected:
 	void ParseCommandLineArguments (int argc, char **argv);
 
 	/**
-   * \brief Configure default attributes
-   * \return none
-   */
-  void ConfigureDefaults ();
+	 * \brief Configure default attributes
+	 * \return none
+	 */
+	void ConfigureDefaults ();
 
 	/**
-   * \brief Configure nodes
-   * \return none
-   */
-  void ConfigureNodes ();
+	 * \brief Configure nodes
+	 * \return none
+	 */
+	void ConfigureNodes ();
 
 	/**
-   * \brief Configure devices
-   * \return none
-   */
-  void ConfigureDevices ();
+	 * \brief Configure devices
+	 * \return none
+	 */
+	void ConfigureDevices ();
 
-  /**
-   * \brief Configure mobility
-   * \return none
-   */
-  void ConfigureMobility ();
+	/**
+	 * \brief Configure mobility
+	 * \return none
+	 */
+	void ConfigureMobility ();
 
 	/**
 	 * \brief Set up the adhoc devices
@@ -112,47 +112,59 @@ protected:
 	void SetupAdhocDevices ();
 
 	/**
+	 * \brief Configure connections
+	 * \return none
+	 */
+	void ConfigureConnections ();
+
+	/**
 	 * \brief Set up Fast Broadcast protocol
 	 * \return none
 	 */
 	void SetupFBProtocol ();
 
 	/**
-   * \brief Configure tracing and logging
-   * \return none
-   */
-  void ConfigureTracingAndLogging ();
+	 * \brief Configure tracing and logging
+	 * \return none
+	 */
+	void ConfigureTracingAndLogging ();
 
 	/**
-   * \brief Run the simulation
-   * \return none
-   */
-  void RunSimulation ();
+	 * \brief Schedule FB protocol phases
+	 * \return none
+	 */
+	void ScheduleFBProtocol ();
 
-  /**
-   * \brief Process outputs
-   * \return none
-   */
+	/**
+	 * \brief Run the simulation
+	 * \return none
+	 */
+	void RunSimulation ();
+
+	/**
+	 * \brief Process outputs
+	 * \return none
+	 */
 	void ProcessOutputs ();
 
 private:
-  /**
-   * \brief Run the simulation
-   * \return none
-   */
-  void Run ();
-
-  /**
-   * \brief Run the simulation
-   * \return none
-   */
-  void CommandSetup (int argc, char **argv);
+	/**
+	 * \brief Run the simulation
+	 * \return none
+	 */
+	void Run ();
 
 	/**
-   * \brief Set up a prescribed scenario
-   * \return none
-   */
-  void SetupScenario ();
+	 * \brief Run the simulation
+	 * \return none
+	 */
+	void CommandSetup (int argc, char **argv);
+
+	/**
+	 * \brief Set up a prescribed scenario
+	 * \return none
+	 */
+	void SetupScenario ();
 
 	/**
 	 * \brief Set up Fast Broadcast protocol parameters for a node
@@ -165,21 +177,24 @@ private:
 	uint32_t 								m_nNodes;
 	NodeContainer						m_adhocNodes;
 	NetDeviceContainer			m_adhocDevices;
+	Ipv4InterfaceContainer	m_adhocInterfaces;
 	uint32_t								m_actualRange;
 	uint32_t								m_estimatedRange;
 	bool										m_flooding;
 	uint32_t								m_rCirc;
 	std::string 						m_rate;
-  std::string 						m_phyMode;
+	std::string 						m_phyMode;
 	uint32_t 								m_mobility;
 	uint32_t 								m_scenario;
 	std::vector <uint32_t>	m_lineNodePositions;
+	std::string							m_address;
+	uint32_t								m_port;
 	std::string 						m_CSVfileName;
 	double									m_TotalSimTime;
 };
 
 FBVanetExperiment::FBVanetExperiment ()
-  : m_txp (7.5),
+	: m_txp (7.5),
 		m_nNodes (10),
 		m_actualRange (300),
 		m_estimatedRange (0),
@@ -187,6 +202,10 @@ FBVanetExperiment::FBVanetExperiment ()
 		m_rCirc (1000),
 		m_rate ("2048bps"),
 		m_phyMode ("DsssRate11Mbps"),
+		m_mobility (1),
+		m_scenario (1),
+		m_address ("10.1.255.255"),
+		m_port (9),
 		m_CSVfileName ("manet-routing.output.csv"),
 		m_TotalSimTime (300.01)
 {
@@ -204,15 +223,19 @@ FBVanetExperiment::Simulate (int argc, char **argv)
 	NS_LOG_INFO ("Enter 'FB Vanet Experiment' enviroment.");
 
 	// Initial configuration and parameters parsing
-  ParseCommandLineArguments (argc, argv);
+	ParseCommandLineArguments (argc, argv);
 	ConfigureDefaults ();
 
-	// Configure the network and the elements within
+	// Configure the network and all the elements in it
 	ConfigureNodes ();
 	ConfigureMobility ();
 	SetupAdhocDevices ();
+	ConfigureConnections ();
 	SetupFBProtocol ();
 	ConfigureTracingAndLogging ();
+
+	// Schedule FB protocol phases
+	ScheduleFBProtocol ();
 
 	// Run simulation and print some results
 	RunSimulation ();
@@ -279,6 +302,8 @@ FBVanetExperiment::ConfigureMobility ()
 void
 FBVanetExperiment::SetupAdhocDevices ()
 {
+	NS_LOG_INFO ("Configure channels.");
+
 	// Setting up wifi phy and channel using helpers
 	WifiHelper wifi;
 	wifi.SetStandard (WIFI_PHY_STANDARD_80211b);
@@ -303,6 +328,45 @@ FBVanetExperiment::SetupAdhocDevices ()
 }
 
 void
+FBVanetExperiment::ConfigureConnections ()
+{
+	NS_LOG_INFO ("Configure Internet stack.");
+
+	InternetStackHelper internet;
+	internet.Install (m_adhocNodes);
+
+	Ipv4AddressHelper addressAdhoc;
+	addressAdhoc.SetBase ("10.1.0.0", "255.255.0.0");	// TODO: make them variable
+
+	m_adhocInterfaces = addressAdhoc.Assign (m_adhocDevices);
+
+	OnOffHelper onoff1 ("ns3::UdpSocketFactory", Address ());
+	onoff1.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1.0]"));
+	onoff1.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0.0]"));
+
+	NS_LOG_INFO ("Configure connections.");
+
+	// Set receiver (for each node)
+	for (uint32_t j = 0; j < m_nNodes; j++)
+	{
+		//Ptr<Socket> sink = SetupPacketReceive (m_adhocInterfaces.GetAddress (j), m_adhocNodes.Get (j));	// TODO: add SetupPacketReceive function
+		AddressValue remoteAddress (InetSocketAddress (ns3::Ipv4Address::GetAny (), m_port));
+		onoff1.SetAttribute ("Remote", remoteAddress);
+	}
+
+	// Set unicast sender
+	// TODO: add SetupPacketSend function
+	// for (uint32_t i = 0; i < m_nNodes; i++)
+	// {
+	// 	Ptr<Socket> sender = SetupPacketSend (ns3::Ipv4Address(m_address.c_str ()),  m_adhocNodes.Get (i));
+	// 	sender->SetAllowBroadcast (true);
+	//
+	// 	// Add socket to the node
+	// 	m_adhocNodes.Get(i)->setBroadcast(sender);
+	// }
+}
+
+void
 FBVanetExperiment::SetupFBProtocol ()
 {
 	// Configure FB protocol parameters for all nodes
@@ -316,12 +380,21 @@ void
 FBVanetExperiment::ConfigureTracingAndLogging ()
 {
 	// Enable logging from the ns2 helper
-  LogComponentEnable ("Ns2MobilityHelper", LOG_LEVEL_DEBUG);
+	LogComponentEnable ("Ns2MobilityHelper", LOG_LEVEL_DEBUG);
 
 	Packet::EnablePrinting ();
 }
 
-
+void
+FBVanetExperiment::ScheduleFBProtocol ()
+{
+	// TODO
+	// // Hello messages
+	// Simulator::Schedule (Seconds (500), &RoutingExperiment::Hello, adhocNodes, 60);
+	//
+	// // Generate alert message
+	// Simulator::ScheduleWithContext (adhocNodes.Get (start)->GetId (), Seconds (45000), &RoutingExperiment::GenerateAlertTraffic, adhocNodes.Get (start), adhocNodes);
+}
 
 void
 FBVanetExperiment::CommandSetup (int argc, char **argv)
@@ -337,9 +410,11 @@ FBVanetExperiment::CommandSetup (int argc, char **argv)
 	cmd.AddValue ("rCirc", "Vehicles transimision area" , m_rCirc);
 	cmd.AddValue ("rate", "Rate", m_rate);
 	cmd.AddValue ("phyModeB", "Phy mode 802.11b", m_phyMode);
-	cmd.AddValue ("mobility", "1=stationary, 2=moving", m_mobility);
+	cmd.AddValue ("mobility", "Node mobility: 1=stationary, 2=moving", m_mobility);
 	// cmd.AddValue ("speed", "Node speed (m/s)", m_nodeSpeed);
 	cmd.AddValue ("scenario", "1=straight street, 2=grid layout, 3=real world", m_scenario);
+	cmd.AddValue ("address", "IP address", m_address);
+	cmd.AddValue ("port", "IP port", m_port);
 	cmd.AddValue ("CSVfileName", "The name of the CSV output file name", m_CSVfileName);
 	cmd.AddValue ("totaltime", "Simulation end time", m_TotalSimTime);
 
@@ -357,6 +432,7 @@ FBVanetExperiment::SetupScenario ()
 		m_nNodes = 10;
 
 		// Node positions (in meters) along the straight street (or line)
+		m_lineNodePositions.resize (10, 0);
 		m_lineNodePositions[0] = 100;
 		m_lineNodePositions[1] = 300;
 		m_lineNodePositions[2] = 500;
@@ -397,7 +473,7 @@ FBVanetExperiment::SetupFBParameters (Ptr<Node> node)
 void
 FBVanetExperiment::RunSimulation ()
 {
-  Run ();
+	Run ();
 }
 
 void
@@ -405,12 +481,13 @@ FBVanetExperiment::ProcessOutputs ()
 {
 	NS_LOG_INFO ("Process outputs.");
 
+	// TODO
 }
 
 void
 FBVanetExperiment::Run ()
 {
-  NS_LOG_INFO ("Run simulation.");
+	NS_LOG_INFO ("Run simulation.");
 
 	// Setup netanim config ?
 	Simulator::Stop (Seconds (m_TotalSimTime));
@@ -422,5 +499,5 @@ FBVanetExperiment::Run ()
 int main (int argc, char *argv[])
 {
 	FBVanetExperiment experiment;
-  experiment.Simulate (argc, argv);
+	experiment.Simulate (argc, argv);
 }
