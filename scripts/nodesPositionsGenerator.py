dbb9c4fca4ed981f1adce3b5d2a6c34d9ef0d21c
Marco Romanelli <mromanelli9@gmail.com>
2017-09-22 18:05:34 +0200

    done

--- scripts/nodesPositionsGenerator.py
+++ scripts/nodesPositionsGenerator.py
@@ -20,7 +20,6 @@
 #
 #
 
-
 """
 A script to generate ns3::Nodes positions, given a bounding box and "no-fly zones"
 
@@ -31,27 +30,138 @@ A script to generate ns3::Nodes positions, given a bounding box and "no-fly zone
 """
 
 import getopt, sys
+import xml.etree.ElementTree as ET
+import random
+from decimal import Decimal
 
-def main(argv):
+def parsePolyFile(filename):
+	xmlTree = ET.parse(filename)
+	saved = []
+
+	for el in xmlTree.iter():
+		if el.tag == "poly" and ("building" in " ".join( [v for v in el.attrib.values()])):
+			saved.append(el)
+
+	return saved
+
+def extractNoflyZones(polyData):
+	zones = []
+
+	for i, osmEl in enumerate(polyData):
+		polyEl = polyData[i]
+
+		current = shapeToBounds(polyEl.attrib["shape"])
+		zones.append(current)
+
+	return zones
+
+def shapeToBounds(shape):
+	bounds = {
+		"xMin": -1,
+		"xMax": -1,
+		"yMin": -1,
+		"yMax": -1,
+		"zMin": -1,
+		"zMax": -1,
+	}
+
+	for point in shape.split(' '):
+		x, y = point.split(',')
+		xNum = float (x)
+		yNum = float (y)
+
+		# Min
+		if bounds["xMin"] == -1 or xNum < bounds["xMin"]:
+			bounds["xMin"] = xNum
+		if bounds["yMin"] == -1 or yNum < bounds["yMin"]:
+			bounds["yMin"] = yNum
+
+		# Max
+		if bounds["xMax"] == -1 or xNum > bounds["xMax"]:
+			bounds["xMax"] = xNum
+		if bounds["xMax"] == -1 or yNum > bounds["yMax"]:
+			bounds["yMax"] = yNum
+
+	return bounds
+
+def generateRandomPoints(n, boundingBox, noflyZones):
+	allinone = []
+
+	for i in range(0, n):
+		point = (-1,-1,-1)
+		while (True):
+			# Generate a point within the bounding box
+			x = float(format(Decimal(random.uniform(boundingBox["xMin"], boundingBox["xMax"])), '.2f'))
+			y = float(format(Decimal(random.uniform(boundingBox["yMin"], boundingBox["yMax"])), '.2f'))
+			z = float(format(Decimal(random.uniform(boundingBox["zMin"], boundingBox["zMax"])), '.2f'))
+			point = (x,y,z)
+
+			# Check if the point violate a no-fly zones
+			for zone in noflyZones:
+				if point[0] < zone["xMin"] or point[0] > zone["xMax"]:
+					continue
+				if point[1] < zone["yMin"] or point[1] > zone["yMax"]:
+					continue
+				if point[2] < zone["zMin"] or point[2] > zone["yMax"]:
+					continue
+
+			# If the point passed all the tests, then is ok
+			break
+
+		allinone.append((x,y,z))
+
+	return allinone
+
+def storeInFile(filename, data):
+	with open(filename, 'w') as f:
+		for point in data:
+			val = str(point[0]) + '\t' + str(point[1]) + '\t' + str(point[2]) + '\n'
+			f.write(val)
+
+def main(argv, boundingBox, outputFile):
 	# Parse args
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "", ["poly-file="])
+		opts, args = getopt.getopt(argv, "", ["poly-file=", "nodes="])
 	except getopt.GetoptError as err:
 		# print help information and exit:
-		print( str( err ) )  # will print something like "option -a not recognized"
+		print(str(err))  # will print something like "option -a not recognized"
 		sys.exit(2)
 
-	osmFilepath = polyFilepath = None
+	polyFilepath = None
+	N = 1
 	for o, a in opts:
-		if o in ("--poly-file"):
+		if o in ("--nodes"):
+			N = int(a)
+		elif o in ("--poly-file"):
 			polyFilepath = str(a)
 		else:
 			assert False, "[!] Unhandled option."
 
 	assert polyFilepath != None, "[!] Missing inputs."
 
-	# Parse osm file
-	print(polyFilepath)
+	# Parse poly file
+	polydata = parsePolyFile(polyFilepath)
+
+	# Extract building data
+	zones = extractNoflyZones(polydata)
+
+	# Generate random point
+	points = generateRandomPoints(N, boundingBox, zones)
+
+	# Save
+	storeInFile(outputFile, points)
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+	# Bounding box aka word bounds
+	boundingBox = {
+		"xMin": 0,
+		"xMax": 300,
+		"yMin": 0,
+		"yMax": 300,
+		"zMin": 70,
+		"zMax": 100,
+	}
+
+	outputFile = "nodes_coordinates.txt"
+
+	main(sys.argv[1:], boundingBox, outputFile)
